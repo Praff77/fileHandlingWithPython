f781b95fbf60147b8ab68f71c780d4935d349ac2
Prafull Nikam <prafullinside@gmail.com>
2019-11-26 13:00:24 +1100

    last modification time of a file, path and OS identification

--- ospathutils_start.py
+++ ospathutils_start.py
@@ -10,20 +10,23 @@ import time
 
 def main():
   # Print the name of the OS
-
+  print(os.name)
 
   # Check for item existence and type
-
-  
+  print("item eXist " + str(path.exists("textfile.txt")))
+  print("ithem is a file: " +str(path.isfile("textfile.txt")))
+  print("is a directory " +str(path.isdir("textfile.txt")))
   # Work with file paths
-
-  
+  print("item path: " + str(path.realpath("textfile.txt")))
+  print("item path and name: " + str(path.split(path.realpath("textfile.txt"))))
   # Get the modification time
-
+  t = time.ctime(path.getmtime("textfile.txt"))
+  print(t)
+  print(datetime.datetime.fromtimestamp(path.getmtime("textfile.txt")))
   
   # Calculate how long ago the item was modified
-
-
-  
+  td = datetime.datetime.now() - datetime.datetime.fromtimestamp(path.getmtime("textfile.txt"))
+  print("it has been" + str(td) + "since the file was modiefied")
+  print("or, " + str(td.total_seconds()) + "seconds")  
 if __name__ == "__main__":
   main()
